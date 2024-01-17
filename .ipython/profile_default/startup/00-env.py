7db464bba00f0f2cb9f06dcc1ad59b1e57f0699b
edwincamirtharaj <edwincamirtharaj@gmail.com>
2024-01-17 12:46:59 +0000

    first commit From PAW

---
+++ .ipython/profile_default/startup/00-env.py
@@ -0,0 +1,13 @@
+# *WARNING* Do not change this file as it may break your notebooks
+
+import os
+os.environ['HOME'] = '/home/edwincamirtharaj'
+os.environ['LC_CTYPE'] = 'en_US.utf-8'
+os.environ['JAVA_HOME'] = '/usr/lib/jvm/default-java'
+os.environ['TERM'] = 'xterm-256color'
+os.environ['PYTHONSTARTUP'] = '/home/edwincamirtharaj/.pythonstartup.py'
+os.environ['PYTHONANYWHERE_DOMAIN'] = 'pythonanywhere.com'
+os.environ['PYTHONANYWHERE_SITE'] = 'www.pythonanywhere.com'
+os.environ['API_TOKEN'] = '2ebc8dc67de5f54bc1a92ac0d5b84d09a593a603'
+os.environ.pop("http_proxy", None)
+os.environ.pop("https_proxy", None)
